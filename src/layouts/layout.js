93c6e7273248de91ad1f97e0f9eff94da3fe00f5
shawn-nyquist <shawn.nyquist@gmail.com>
2019-03-23 16:57:48 -0500

    Finished Activity. Throwing error

--- src/layouts/layout.js
+++ src/layouts/layout.js
@@ -4,11 +4,75 @@ import { StaticQuery, graphql } from 'gatsby'
 import './layout.css'
 import { ThemeProvider, ThemeConsumer } from 'styled-components'
 import { Gray as theme } from '../themes/Gray'
-import { Footer, Main } from '../components/Element'
-import { Masthead, DefaultLayout } from '../components/Masthead'
+import { Footer, Link, Main } from '../components/Element'
+import { Masthead, MastheadDrawer, DefaultLayout } from '../components/Masthead'
 
-const Layout = ({ children }) => (
-  <ThemeProvider theme={theme}>
+const links = [
+  {
+    variant: 'contrast',
+    to: 'health-medicine',
+    text: 'Health'
+  },
+  {
+    variant: 'contrast',
+    to: 'mind-brain',
+    text: 'Mind'
+  },
+  {
+    variant: 'contrast',
+    to: 'technology',
+    text: 'Technology'
+  },
+  {
+    variant: 'contrast',
+    to: 'space-physics',
+    text: 'Space'
+  },
+  {
+    variant: 'contrast',
+    to: 'environment',
+    text: 'Environment'
+  }
+]
+
+
+export default class Layout extends React.Component {
+  state = {
+    drawerOpen: true,
+    prevScrollPosition: null
+  };
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.checkScrollPosition.bind(this))
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.checkScrollPosition.bind(this))
+  }
+
+  checkScrollPosition() {
+    let offset = 100
+    let { drawerOpen } = this.state
+    let scrolledDown = window.scrollY > this.prevScrollPosition
+
+    scrolledDown && window.scrollY > offset
+      ? drawerOpen &&
+        this.setState({ drawerOpen: false, showScrollupImage: true})
+      : !drawerOpen &&
+        this.setState({ drawerOpen: true, showScrollupImage: false});
+
+    if (window.matchMedia('(max-width: 768px)').matches) {
+      this.setState({ showScrollupImage: false });
+    }
+    
+    this.prevScrollPosition = window.scrollY;
+  }
+
+  render() {
+    const { children } = this.props;
+
+    return (
+      <ThemeProvider theme={theme}>
     <StaticQuery
       query={graphql`
         query SiteTitleQuery {
@@ -23,32 +87,44 @@ const Layout = ({ children }) => (
         <div>
           <Masthead height="85px">
             <ThemeConsumer>
-              {theme => <DefaultLayout image={theme.images.mastheadImage} />}
+              {theme =>( <DefaultLayout image={theme.images.mastheadImage} />
+              )}
             </ThemeConsumer>
           </Masthead>
-          <div
-            style={{
-              margin: `0 auto`,
-              maxWidth: 960,
-              padding: `0px 1.0875rem 1.45rem`,
-              paddingTop: 0
-            }}
-          >
-            <Main mt={85}>{children}</Main>
-            <Footer>
-              © {new Date().getFullYear()}, Built with
-              {` `}
-              <a href="https://www.gatsbyjs.org">Gatsby</a>
-            </Footer>
-          </div>
-        </div>
+          <Main mt={85}>
+            <MastheadDrawer 
+              height="56px" 
+              width="900px" 
+              top="85px" 
+              open={this.state.drawerOpen}
+            >
+              {links.map((link, i) => (
+                <Link 
+                  key={i}
+                  variant={link.variant}
+                  href={link.to}
+                  fontSize={[0, 1, 2]}
+                >
+                    {link.text}
+                  </Link>
+              ))}
+            </MastheadDrawer>
+            {children}
+          </Main>
+          <Footer>
+            © {new Date().getFullYear()}, Built with
+            {` `}
+            <a href="https://www.gatsbyjs.org">Gatsby</a>
+          </Footer>
+        </div>    
       )}
     />
   </ThemeProvider>
-)
+    )
+  }
+}
+
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired
-}
-
-export default Layout
+};
