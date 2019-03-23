bb537f04e9eaea46eb3e930f06369bf308320504
shawn-nyquist <shawn.nyquist@gmail.com>
2019-03-23 14:56:01 -0500

    Added breakpoints

--- src/themes/Gray/Gray.js
+++ src/themes/Gray/Gray.js
@@ -12,7 +12,10 @@ const icons = {
   Search: <Search />
 }
 
+const breakpoints = [576, 768, 900]
+
 const theme = {
+  breakpoints: [...breakpoints],
   space: [0, 4, 8, 16, 32, 64, 128, 256, 512],
   lineHeights: {
     solid: 1,
@@ -47,6 +50,13 @@ const theme = {
       secondary: 'hsl(208, 13%, 45%)',
       disabled: 'hsl(208, 13%, 75%)',
       hint: 'hsl(208, 13%, 75%)'
+    }
+  },
+  media: {
+    sizes: {
+      desktop: breakpoints[2],
+      tablet: breakpoints[1],
+      phone: breakpoints[0]
     }
   }
 }
